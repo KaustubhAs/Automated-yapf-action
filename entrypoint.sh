e2f7f6e268d4a58eff76deadae587ef024253624
Kaustubh Sonawane <83910712+KaustubhAs@users.noreply.github.com>
2024-11-25 17:08:42 +0000

    changes to entrypoint added to solve the error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
-# Execute YAPF with all passed arguments
-yapf "$@"
+#!/bin/sh -l
+set -uo pipefail
 
-# Capture exit code and set it as output
-exit_code=$?
-echo "exit-code=$exit_code" >> $GITHUB_ENV
-exit $exit_code
+yapf $*
+echo ::set-output name=exit-code::$?
+
+# # Execute YAPF with all passed arguments
+# yapf "$@"
+
+# # Capture exit code and set it as output
+# exit_code=$?
+# echo "exit-code=$exit_code" >> $GITHUB_ENV
+# exit $exit_code
