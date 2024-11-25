e3f79a5d3b5e92b9063411ab4b957a0222a7ab3d
kaustubhAS <kaustubhsonawane13@gmail.com>
2024-11-25 18:53:29 +0000

    Automated yapf fixes

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-def example_function(x,y):
-  if(x>0):
-   print("Positive")
-  elif(x==0):print("Zero")
-  else:
-      print("Negative")
-  for i in range(0,10):
-      if i%2==0:print(f"{i} is even")
-      else:
-       print(f"{i} is odd")
-  return x+y
+def example_function(x, y):
+    if (x > 0):
+        print("Positive")
+    elif (x == 0):
+        print("Zero")
+    else:
+        print("Negative")
+    for i in range(0, 10):
+        if i % 2 == 0: print(f"{i} is even")
+        else:
+            print(f"{i} is odd")
+    return x + y
